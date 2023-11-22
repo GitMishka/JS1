6b7bd4345b9055e2ecfe231ab5685faee0ab8ddf
Misha <mishagrigoryan@gmail.com>
2023-11-22 18:00:04 -0500

    2

--- 1.js
+++ 1.js
@@ -2,5 +2,5 @@ function isEven(number) {
     return number % 2 === 0;
 }
 
-console.log(isEven(4)); // This will output: true
-console.log(isEven(5)); // This will output: false
+console.log(isEven(4));
+console.log(isEven(5)); 
