73e86df76848fb5dee7c373e30985a133773e623
Misha <mishagrigoryan@gmail.com>
2023-11-23 23:39:55 -0500

    3

--- 1.js
+++ 1.js
@@ -1,7 +1,9 @@
 const temperatures = [3,-2,-6,-1,'error',9, 13, 17, 15, 14, 9, 5];
 
 
-const calcTempAmplitude = function (temps) {
+const calcTempAmplitude = function (t1,t2) {
+
+    const temps = t1.concat(t2)    
     let max = temps[0];
     let min = temps[0];
     for (let i = 0; i < temps.length; i++) {
@@ -13,7 +15,7 @@ const calcTempAmplitude = function (temps) {
     console.log(max, min);
     return max - min;
 };
-const amplitude = calcTempAmplitude(temperatures)
+const amplitude = calcTempAmplitude([3,5,1],[9,0,5])
 console.log(amplitude)
 // const array = [1, 2, 3, 4, 5];
 // const maxValue = Math.max(...array);
